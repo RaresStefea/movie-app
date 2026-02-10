ce9277b43768f40d1518f4972c216f266bccd143
Rares <Rares.Stefea@cognizant.com>
2026-02-10 19:51:30 +0200

    added footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,1 +1,16 @@
-export default function Footer() {}
+import React from "react";
+import style from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={style.siteFooter} role="contentinfo">
+      <div className={style.footerInner}>
+        <span className={style.brand}>movier</span>
+        <span className={style.dot} aria-hidden="true">
+          @
+        </span>
+        <span className={style.year}>2026</span>
+      </div>
+    </footer>
+  );
+}
