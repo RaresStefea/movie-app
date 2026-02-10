3e5caa962f552f950510197e5a80a918cba25935
Rares <Rares.Stefea@cognizant.com>
2026-02-10 14:25:45 +0200

    Finished navbar design, added barrels and polished components.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,14 @@
+import React from "react";
+import { Navbar, Footer, CardList } from "../components/index.js";
+import styles from "./App.module.css";
+
 export default function App() {
-  return <h1> Movier</h1>;
+  return (
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <CardList title="Example" />
+      </main>
+    </>
+  );
 }
